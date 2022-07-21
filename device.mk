6271d63fd99c1eebae6fe0a2c1202c674cf4386e
Anay Wadhera <awadhera@berkeley.edu>
2022-07-21 19:20:47 +0000

    porsche: Consolidate overlays
    [mvaisakh]: Change to RROs
    
    Change-Id: Idf17d496bc3b6462eb995d8fc104e6040998abf5
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- device.mk
+++ device.mk
@@ -301,13 +301,11 @@ PRODUCT_PACKAGES += \
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     PorscheCarrierConfigOverlay \
     PorscheFrameworksOverlay \
+    PorscheOPlusDozeResTarget \
     PorscheOPlusFrameworksOverlay \
     PorscheOPlusSettingsOverlay \
     PorscheOPlusSystemUIOverlay \
